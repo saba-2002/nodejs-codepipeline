2097f53fe2afa0d7587516c5ef7eca8500c74edd
ashi-2212 <ashi.singh@appinventiv.com>
2024-09-02 15:32:17 +0530

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return hello world', (done) => {
     request(app)
       .get('/')
-      .expect('Chaitanya Sharma', done);
+      .expect('Ashi Singh', done);
   });
 });
