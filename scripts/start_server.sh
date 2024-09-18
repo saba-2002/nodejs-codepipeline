95dd1d50ee2a4f2b18c5da0d0d6b978d24cd4fd9
ashi-2212 <ashi.singh@appinventiv.com>
2024-09-19 01:00:30 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-forever start /var/nodeapp/src/index.js
+
+# Stop any running container
+if [ "$(docker ps -q -f name=node-app)" ]; then
+    docker stop node-app
+    docker rm node-app
+fi
+
+# Pull the latest image from ECR
+$(aws ecr get-login --no-include-email --region eu-north-1)
+docker pull 339712999129.dkr.ecr.eu-north-1.amazonaws.com/node-app:latest
+
+# Run the new container
+docker run -d -p 3000:3000 --name node-app 339712999129.dkr.ecr.eu-north-1.amazonaws.com/node-app:latest
+
+
+
+# forever start /var/nodeapp/src/index.js
