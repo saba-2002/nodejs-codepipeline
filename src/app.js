0624eed9b054ed4d4f3286dd2b7e93942ddd9d43
saba-2002 <saba.khan@appinventiv.com>
2024-09-19 13:12:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Ashi Singh');
+  res.send('Its Saba ...hehehehhe');
 });
 
 module.exports = app;
